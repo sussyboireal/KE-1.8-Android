260ae0c7568f441a3ecdf4b93c4ff55c1962d7d0
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-06 14:38:55 +0200

    Update CastomAndroidControls.hx

--- source/CastomAndroidControls.hx
+++ source/CastomAndroidControls.hx
@@ -68,7 +68,7 @@ class CastomAndroidControls extends MusicBeatState
 		exitbutton.color = FlxColor.fromRGB(255,0,0);
 		add(exitbutton);		
 
-		var savebutton = new FlxButton(exitbutton.x, exitbutton.y + 100, "Save And Exit", function()
+		var savebutton = new FlxButton(exitbutton.x, exitbutton.y + 100, "Save", function()
 		{
 			save();
 			FlxG.switchState(new OptionsDirect());
